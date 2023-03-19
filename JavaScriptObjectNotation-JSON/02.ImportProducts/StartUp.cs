8e7e370926be3d4b0cde257fae3bddd0000cf08c
Simeon Kirov <simeonkirov21@gmail.com>
2023-03-19 15:25:01 +0200

    02.ImportProducts 100/100

---
+++ JavaScriptObjectNotation-JSON/02.ImportProducts/StartUp.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using ProductShop.Data;
+using ProductShop.DTOs;
+using ProductShop.DTOs.Export;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            string jsonFilePath = File.ReadAllText("../../../Datasets/products.json");
+
+            Console.WriteLine(ImportProducts(context, jsonFilePath));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = context.Users;
+
+            var data = JsonConvert.DeserializeObject<List<User>>(inputJson);
+
+            users.AddRange(data);
+            context.SaveChanges();
+
+            return $"Successfully imported {data.Count}";
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            IMapper mapper = CreateMapper();
+
+            ProductDto[] productDtos =
+                JsonConvert.DeserializeObject<ProductDto[]>(inputJson);
+            Product[] products = mapper.Map<Product[]>(productDtos);
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Length}";
+        }
+
+        private static IMapper CreateMapper()
+        {
+            return new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            }));
+        }
+
+        private static IContractResolver ConfigureCamelCaseNaming()
+        {
+            return new DefaultContractResolver()
+            {
+                NamingStrategy = new CamelCaseNamingStrategy(false, true)
+            };
+        }
+    }
+}
